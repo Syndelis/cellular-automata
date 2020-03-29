b9d8462438ff4e2e7d65899dd6aa3beead88d29d
Brenno Lemos <brenno@syndel.is>
2020-03-29 14:00:03 -0300

    Changed CA.events() so that passing in a float returns a boolean rather than a list containing a single boolean

--- ca.pyx
+++ ca.pyx
@@ -308,14 +308,19 @@ cdef class CA:
        """
        odds_l = []
         # Test odds values to verify negative values?
+
        if isinstance(odds, float):
-           if random() < odds: return [True]
-           else: return [False]
+
+           if random() < odds: return True
+           else: return False
+
        elif isinstance(odds, Iterable):
+
            for i in odds:
                if random() < i: odds_l.append(True)
                else: odds_l.append(False)
            return odds_l
+
        else: raise TypeError(
                "`odds` parameter must be either an int or "
                "Iterable. Was %s" % str(type(odds)))
